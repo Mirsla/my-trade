7ddae400d40fa92998fd5f4ec9299abd90c380f5
MAIKE51\ziyang <ziyang@maike51.com>
2019-02-22 14:01:53 +0800

    忽略其他配置，先用当作最简单的spring配置启动起来

--- my-trade-service/src/main/java/com/mirsla/trade/TradeServiceApplication.java
+++ my-trade-service/src/main/java/com/mirsla/trade/TradeServiceApplication.java
@@ -1,23 +1,17 @@
 package com.mirsla.trade;
 
-import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
-import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.ApplicationPidFileWriter;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableDiscoveryClient
-@EnableApolloConfig
+//@EnableDiscoveryClient
+//@EnableApolloConfig
 @SpringBootApplication(scanBasePackages = {"com.mirsla.trade"})
-@MapperScan("com.mk.trade.dao")
-@EnableFeignClients(basePackages = {"com.mk.crm.api", "com.mk.product.api", "com.mk.promotion.api", "com.mk.shop.api",
-	"com.mk.tower.api", "com.mk.uic.api", "com.mk.mdata.api", "com.mk.yum.api"})
-@EnableDubboConfiguration
-@EnableHystrix
+//@MapperScan("com.mk.trade.dao")
+//@EnableFeignClients(basePackages = {"com.mk.crm.api", "com.mk.product.api", "com.mk.promotion.api", "com.mk.shop.api",
+//	"com.mk.tower.api", "com.mk.uic.api", "com.mk.mdata.api", "com.mk.yum.api"})
+//@EnableDubboConfiguration
+//@EnableHystrix
 //@EnableConfigurationProperties({MkTradeProperties.class, RedisProperties.class, ExpressConfig.class, ConfigProperties.class})
 //@ImportAutoConfiguration(value = {MoCacheAutoConfiguration.class})
 //@EnableMQConfiguration
